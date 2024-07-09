a07264d4fa87e877686b75fca7f1c4e65ff8a6c7
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-07-09 21:30:45 +0800

    create redux store and state for running the game

---
+++ src/state/runningSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const runningSlice = createSlice({
+    name: 'running',
+    initialState: {
+        value: false,
+    },
+    reducers: {
+        toggleRunning: (state) => {
+            state.value = !state.value
+        }
+    }
+})
+
+export const { toggleRunning } = runningSlice.actions;
+export default runningSlice.reducer;
+
+export const runningValue = (state) => state.running.value;
--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import runningReducer from './runningSlice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        running: runningReducer 
+    },
 })
