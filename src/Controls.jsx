ea3ce1ad02245c7d7a79894f77a1b5ce20334030
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-07-13 12:42:30 +0800

    set up onClick event for stepForward func

--- src/Controls.jsx
+++ src/Controls.jsx
@@ -4,7 +4,7 @@ import { Slider } from './Slider/Slider.jsx'
 import { useDispatch, useSelector } from 'react-redux'
 import { runningValue, toggleRunning } from './state/runningSlice.js'
 
-export const Controls = ({ startGame, runningRef }) => {
+export const Controls = ({ startGame, runningRef, stepForward }) => {
   const dispatch = useDispatch()
   const running = useSelector(runningValue)
   return (
@@ -23,11 +23,16 @@ export const Controls = ({ startGame, runningRef }) => {
 
         {/* play / Stop icons */}
         {running ? (
-          <svg className="controls-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" onClick={() => {
-            if (running) {
-              dispatch(toggleRunning())
-            }
-          }}>
+          <svg
+            className="controls-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            onClick={() => {
+              if (running) {
+                dispatch(toggleRunning())
+              }
+            }}
+          >
             <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z" />
           </svg>
         ) : (
@@ -48,7 +53,12 @@ export const Controls = ({ startGame, runningRef }) => {
         )}
         {/* end of play / Stop icons */}
 
-        <svg className="next-icon controls-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <svg className="next-icon controls-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={() => {
+          if (running) {
+            dispatch(toggleRunning())
+          }
+          stepForward();
+        }}>
           <path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3V256v41.7L52.5 440.6zM256 352V256 128 96c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29V352z" />
         </svg>
       </div>
