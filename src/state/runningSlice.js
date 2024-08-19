ffbd16b9bedb9f6bbffa77445aee9b13399a43a5
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-08-19 21:22:43 +0800

    minor fix

--- src/state/runningSlice.js
+++ src/state/runningSlice.js
@@ -1,18 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const runningSlice = createSlice({
-    name: 'running',
-    initialState: {
-        value: false,
+  name: 'running',
+  initialState: {
+    value: false,
+  },
+  reducers: {
+    toggleRunning: state => {
+      state.value = !state.value
     },
-    reducers: {
-        toggleRunning: (state) => {
-            state.value = !state.value
-        }
-    }
+  },
 })
 
-export const { toggleRunning } = runningSlice.actions;
-export default runningSlice.reducer;
+export const { toggleRunning } = runningSlice.actions
+export default runningSlice.reducer
 
-export const runningValue = (state) => state.running.value;
+export const runningValue = state => state.running.value
