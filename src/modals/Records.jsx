4c160514e5a3b319c8d583ce87ef34e6ecdb80fb
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-08-22 17:08:15 +0800

    set up records functionality

--- src/modals/Records.jsx
+++ src/modals/Records.jsx
@@ -1,24 +1,59 @@
 import styles from './Records.module.css'
+import { useEffect, useState } from 'react'
+import { getFromLocalStorage } from '../helpers/handleLocalStorage.js'
 export const Records = () => {
+  const recordNum = ['1.', '2.', '3.']
+
+  const [records, setRecords] = useState([])
+  const storageRecords = getFromLocalStorage('records')
+
+  useEffect(() => {
+    const getStorageRecords = () => {
+      if (storageRecords) {
+        setRecords(storageRecords)
+      } else {
+        setRecords(null)
+      }
+    }
+    getStorageRecords()
+
+    window.addEventListener('storage', getStorageRecords)
+
+    return () => {
+      window.removeEventListener('storage', getStorageRecords)
+    }
+  }, [])
+
+  const getBestScore = () => {
+    if (!records) {
+      return
+    }
+    const best = Math.max(...records)
+    return best
+  }
+
+  const getLastThreeRecords = () => {
+    if (!records) {
+      return
+    }
+    return records.slice(-3).reverse()
+  }
+
   return (
     <div className={styles.container}>
       <span className="modal-heading">Records</span>
-      <span className={styles.best}>Your best : {} </span>
+      <span className={styles.best}>Your best : {getBestScore()} </span>
       <div className={styles['records-list-container']}>
         <span className={styles.recordsText}>Last 3 tries :</span>
         <ul className={styles['records-list']}>
-          <li className={styles['single-record']}>
-            <span className={styles['record-num']}>1.</span>
-            <span className={styles['record-val']}>{}</span>
-          </li>
-          <li className={styles['single-record']}>
-            <span className={styles['record-num']}>2.</span>
-            <span className={styles['record-val']}>{}</span>
-          </li>
-          <li className={styles['single-record']}>
-            <span className={styles['record-num']}>3.</span>
-            <span className={styles['record-val']}>{}</span>
-          </li>
+          {getLastThreeRecords().map(record => {
+            return (
+              <li className={styles['single-record']} key={record}>
+                <span className={styles['record-num']}>{'-'}</span>
+                <span className={styles['record-val']}>{record}</span>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </div>
