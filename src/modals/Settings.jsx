6519cd12ca0b13b0280956df4f1052d48f165f99
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-08-30 18:33:10 +0800

    fix settings

--- src/modals/Settings.jsx
+++ src/modals/Settings.jsx
@@ -1,34 +1,61 @@
-import styles from './Settings.module.css'
+import styles from './Settings.module.css';
 
-const iconTypes = ['canvas', 'text', 'svg']
-const cellsAxes = ['Y-axis', 'X-axis']
+import { useDispatch, useSelector } from 'react-redux';
+import { setNumRowsCols, numRowsColsValue } from '../state/numRowsColsSlice';
+
+const iconTypes = ['canvas', 'text', 'svg'];
+const cellsAxes = [
+  ['X-axis', 'cols'],
+  ['Y-axis', 'rows'],
+];
 
 export const Settings = () => {
+  const dispatch = useDispatch();
+  const numColsRowsVal = useSelector(numRowsColsValue);
+  
   return (
     <div>
-      <span className="modal-heading">Settings</span>
+      <span className='modal-heading'>Settings</span>
       {/* icon type */}
       <div className={styles['settings-lists']}>
+        <span className={styles.subheading}>Icon Type :</span>
         <dl className={styles['single-list']}>
-          <span className={styles.subheading}>Icon Type :</span>
           {iconTypes.map(iconType => (
-            <dt className={styles['single-option']} key={iconType}>
-              <input type="radio" name="icon-type" id={iconType} defaultChecked />
+            <dt
+              className={styles['single-option']}
+              key={iconType}
+            >
+              <input
+                type='radio'
+                name='icon-type'
+                id={iconType}
+                defaultChecked
+              />
               <label htmlFor={iconType}>{iconType}</label>
             </dt>
           ))}
         </dl>
         {/* cells number */}
-        <dl className={styles['single-list']}>
-          <span className={styles.subheading}>Number of cells :</span>
+        <span className={styles.subheading}>Number of cells :</span>
+        <dl className={`${styles['single-list']} ${styles['cells-list']}`}>
           {cellsAxes.map(axis => (
-            <dt className={styles['cells-number-wrapper']} key={axis}>
-              <label htmlFor={axis}>{axis}</label>
-              <input type="number" name={axis} id={axis} />
+            <dt
+              className={styles['cells-number-wrapper']}
+              key={axis[1]}
+            >
+              <label htmlFor={axis[0]}>{axis[0]}</label>
+              <input
+                className={styles['cells-number-input']}
+                type='number'
+                name={axis[1]}
+                id={axis[1]}
+                value={numColsRowsVal[axis[1]]}
+                onInput={e => dispatch(setNumRowsCols({ [axis[1]]: e.target.value }))}
+              />
             </dt>
           ))}
         </dl>
       </div>
     </div>
-  )
-}
+  );
+};
