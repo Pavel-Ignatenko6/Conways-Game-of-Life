83539d1407ac2a10c624130a375f1b8d697f1883
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-09-05 21:12:04 +0800

    update runningSlice

--- src/state/runningSlice.js
+++ src/state/runningSlice.js
@@ -9,10 +9,13 @@ const runningSlice = createSlice({
     toggleRunning: state => {
       state.value = !state.value
     },
+    setRunning: (state, action) => {
+      state.value = action.payload
+    }
   },
 })
 
-export const { toggleRunning } = runningSlice.actions
+export const { toggleRunning, setRunning } = runningSlice.actions
 export default runningSlice.reducer
 
 export const runningValue = state => state.running.value
