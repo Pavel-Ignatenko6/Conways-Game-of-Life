b99cac4e2ba4825d93659114c3c724f2061f7e49
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-09-05 11:07:15 +0800

    clean the code

--- src/ButtonsPanel.jsx
+++ src/ButtonsPanel.jsx
@@ -1,39 +1,47 @@
-import './styleButtonsPanel.css'
-import { Link } from 'react-router-dom'
+import './styleButtonsPanel.css';
+import { Link } from 'react-router-dom';
 
-import { useDispatch, useSelector } from 'react-redux'
-import { runningValue, toggleRunning } from './state/runningSlice.js'
-import { resetGen } from './state/generationCountSlice.js'
-import { toggleShowModal } from './state/showModalSlice.js'
+import { useDispatch, useSelector } from 'react-redux';
+import { runningValue, toggleRunning } from './state/runningSlice.js';
+import { resetGen } from './state/generationCountSlice.js';
 
 export const ButtonsPanel = ({ resetGameField, setGridHandler }) => {
-  const dispatch = useDispatch()
-  const running = useSelector(runningValue)
+  const dispatch = useDispatch();
+  const running = useSelector(runningValue);
   return (
-    <div className="buttons-panel-background">
-      <div className="buttons-panel">
-        <Link to="/records" className="records-btn btn link" onClick={() => dispatch(toggleShowModal())}>
-          <span className="link-name">Records</span>
+    <div className='buttons-panel-background'>
+      <div className='buttons-panel'>
+        <Link
+          to='/records'
+          className='records-btn btn link'
+        >
+          <span className='link-name'>Records</span>
         </Link>
-        <Link to="/rules" className="check-rules-btn btn link" onClick={() => dispatch(toggleShowModal())}>
-          <span className="link-name">Check rules</span>
+        <Link
+          to='/rules'
+          className='check-rules-btn btn link'
+        >
+          <span className='link-name'>Check rules</span>
         </Link>
         <button
-          className="clear-btn btn"
+          className='clear-btn btn'
           onClick={() => {
             if (running) {
-              dispatch(toggleRunning(true))
+              dispatch(toggleRunning(true));
             }
-            setGridHandler([resetGameField()])
-            dispatch(resetGen())
+            setGridHandler([resetGameField()]);
+            dispatch(resetGen());
           }}
         >
           Clear
         </button>
-        <Link to="/settings" className="settings-btn btn link" onClick={() => dispatch(toggleShowModal())}>
-          <span className="link-name">Settings</span>
+        <Link
+          to='/settings'
+          className='settings-btn btn link'
+        >
+          <span className='link-name'>Settings</span>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
